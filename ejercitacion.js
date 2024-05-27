7e2db63e84980a109353d4000ed1fbafbcdeae2d
Miguel Eduardo Newan <men18248597@gmail.com>
2024-05-27 20:44:00 -0300

    Mi primer Proyect

---
+++ ejercitacion.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4];
+ar.forEach(element => {
+    console.log(element * 2)} );
+});
+
+
---
+++ js/main.js
@@ -0,0 +1,28 @@
+  const changeColorfdc437=()=>{
+    document.getElementById("lorem").style.color="#fdc437"
+  }
+document.getElementById("color-fdc437").addEventListener("click",changeColorfdc437);
+
+
+const changeColor0c8136=()=>{
+    document.getElementById("lorem").style.color="#0c8136"
+  }
+document.getElementById("color-0c8136").addEventListener("click",changeColor0c8136);
+
+
+const changeColor4adccf=()=>{
+    document.getElementById("lorem").style.color="#4adccf"
+  }
+document.getElementById("color-4adccf").addEventListener("click",changeColor4adccf);
+
+
+const changeColor661f45=()=>{
+    document.getElementById("lorem").style.color="#661f45"
+  }
+document.getElementById("color-661f45").addEventListener("click",changeColor661f45);
+
+
+const changeColorf43e3d=()=>{
+    document.getElementById("lorem").style.color="#f43e3d"
+  }
+document.getElementById("color-f43e3d").addEventListener("click",changeColorf43e3d);
